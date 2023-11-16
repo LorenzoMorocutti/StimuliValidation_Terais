89e77793c561659d536cf07001e6946ce653bd7a
LorenzoMorocutti <lorenzo.morocutti@libero.it>
2023-11-16 19:08:16 +0100

    update for nico

--- stimuli_nico.py
+++ stimuli_nico.py
@@ -36,7 +36,7 @@ if os.path.isdir(images_dir):
     print("folder already exist")
 else:
     os.mkdir(images_dir)
-os.mkdir(experiment_dir)
+    os.mkdir(experiment_dir)
 
 ####### CHANGE THE PATH
 now = datetime.now()
@@ -87,13 +87,13 @@ drawing_enjoyment = 0.0
 drawing_frequency = 0.0
 drawing_percentage = 0.0
 
-difficulty_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-enjoyment_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-likeability_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+difficulty_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+enjoyment_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+likeability_ranking = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
-latency_time = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-total_drawing_time = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-number_of_strokes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+latency_time = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+total_drawing_time = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+number_of_strokes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 
 def save_rankings(n):
@@ -116,21 +116,21 @@ def save_rankings(n):
     if os.path.isfile(path_var):
         with open(path_var) as f:
             data = []
-    reader = ndjson.reader(f)
-    for i in reader:
-        data.append(i)
-    data.append(ranking_data)
+            reader = ndjson.reader(f)
+            for i in reader:
+                data.append(i)
+        data.append(ranking_data)
 
     # Writing items to a ndjson file
-    with open(path_var, 'w') as f:
-        writer = ndjson.writer(f, ensure_ascii=False)
-    for d in data:
-        writer.writerow(d)
+        with open(path_var, 'w') as f:
+            writer = ndjson.writer(f, ensure_ascii=False)
+            for d in data:
+                writer.writerow(d)
 
     else:
         open(path_var, "x")
-    with open(path_var, "w") as file:
-        json.dump(ranking_data, file)
+        with open(path_var, "w") as file:
+            json.dump(ranking_data, file)
 
     return
 
@@ -165,15 +165,15 @@ def drawing_questions(n):
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        difficulty_ranking[n] = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            difficulty_ranking[n] = rating
+            touch = True
 
     blue_window()
 
@@ -203,15 +203,15 @@ def drawing_questions(n):
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        enjoyment_ranking[n] = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            enjoyment_ranking[n] = rating
+            touch = True
 
     blue_window()
 
@@ -245,16 +245,16 @@ def drawing_questions(n):
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    image.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            image.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        likeability_ranking[n] = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            likeability_ranking[n] = rating
+            touch = True
 
     blue_window()
     save_rankings(n)
@@ -394,15 +394,15 @@ def artistic_questions():
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        drawing_enjoyment = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            drawing_enjoyment = rating
+            touch = True
 
     blue_window()
 
@@ -431,15 +431,15 @@ def artistic_questions():
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        drawing_frequency = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            drawing_frequency = rating
+            touch = True
 
     blue_window()
 
@@ -469,15 +469,15 @@ def artistic_questions():
     while touch == False:
         if slider.getMouseResponses():
             rating = slider.getRating()
-    slider.setMarkerPos(rating)
-    button_continue.draw()
-    text.draw()
-    slider.draw()
-    win.flip()
+            slider.setMarkerPos(rating)
+            button_continue.draw()
+            text.draw()
+            slider.draw()
+            win.flip()
 
-    if myMouse.isPressedIn(button_continue):
-        drawing_percentage = rating
-    touch = True
+        if myMouse.isPressedIn(button_continue):
+            drawing_percentage = rating
+            touch = True
 
     return
 
@@ -571,9 +571,9 @@ def main():
     while touch == False:
     # print(myMouse.isPressedIn(button))
 
-    if myMouse.isPressedIn(button):
-        print(myMouse.isPressedIn(button))
-    touch = True
+        if myMouse.isPressedIn(button):
+            print(myMouse.isPressedIn(button))
+            touch = True
 
     text = visual.TextStim(win, text="When you are ready, press the button and\n"
                                      "a subject to be drawn will appear on the screen."
@@ -599,7 +599,7 @@ def main():
     text = visual.TextStim(win, text="Please draw with your finger the...\n", color=(1, 1, 1), pos=(0.0, 11.0),
                            colorSpace='rgb', bold=False, height=2.5, anchorHoriz="center", wrapWidth=400)
 
-    text2 = visual.TextStim(win, text="Scissors", color=(1, -0.7, -0.7), pos=(0.0, -1.0),
+    text2 = visual.TextStim(win, text="Tree", color=(1, -0.7, -0.7), pos=(0.0, -1.0),
                             colorSpace='rgb', bold=True, height=4.5, anchorHoriz="center", wrapWidth=400)
 
     text.draw()
@@ -668,163 +668,6 @@ def main():
     for i in seq:
         drawing_task(i)
 
-    art_results = {
-        'art_enjoyment': drawing_enjoyment,
-        'art_frequency': drawing_frequency,
-        'art_percentage': drawing_percentage
-    }
-
-    rank_results = {
-        'Ambulance_difficulty': difficulty_ranking[0],
-        'Tractor_difficulty': difficulty_ranking[1],
-        'Owl_difficulty': difficulty_ranking[2],
-        'Train_difficulty': difficulty_ranking[3],
-        'Sheep_difficulty': difficulty_ranking[4],
-        'LightBulb_difficulty': difficulty_ranking[5],
-        'Lion_difficulty': difficulty_ranking[6],
-        'BirthdayCake_difficulty': difficulty_ranking[7],
-        'Bee_difficulty': difficulty_ranking[8],
-        'Mermaid_difficulty': difficulty_ranking[9],
-        'Flower_difficulty': difficulty_ranking[10],
-        'Spider_difficulty': difficulty_ranking[11],
-        'Leaf_difficulty': difficulty_ranking[12],
-        'Pizza_difficulty': difficulty_ranking[13],
-        'Face_difficulty': difficulty_ranking[14],
-        'Bus_difficulty': difficulty_ranking[15],
-        'PalmThree_difficulty': difficulty_ranking[16],
-
-        'Ambulance_enjoyment': enjoyment_ranking[0],
-        'Tractor_enjoyment': enjoyment_ranking[1],
-        'Owl_enjoyment': enjoyment_ranking[2],
-        'Train_enjoyment': enjoyment_ranking[3],
-        'Sheep_enjoyment': enjoyment_ranking[4],
-        'LightBulb_enjoyment': enjoyment_ranking[5],
-        'Lion_enjoyment': enjoyment_ranking[6],
-        'BirthdayCake_enjoyment': enjoyment_ranking[7],
-        'Bee_enjoyment': enjoyment_ranking[8],
-        'Mermaid_enjoyment': enjoyment_ranking[9],
-        'Flower_enjoyment': enjoyment_ranking[10],
-        'Spider_enjoyment': enjoyment_ranking[11],
-        'Leaf_enjoyment': enjoyment_ranking[12],
-        'Pizza_enjoyment': enjoyment_ranking[13],
-        'Face_enjoyment': enjoyment_ranking[14],
-        'Bus_enjoyment': enjoyment_ranking[15],
-        'PalmThree_enjoyment': enjoyment_ranking[16],
-
-        'Ambulance_likeability': likeability_ranking[0],
-        'Tractor_likeability': likeability_ranking[1],
-        'Owl_likeability': likeability_ranking[2],
-        'Train_likeability': likeability_ranking[3],
-        'Sheep_likeability': likeability_ranking[4],
-        'LightBulb_likeability': likeability_ranking[5],
-        'Lion_likeability': likeability_ranking[6],
-        'BirthdayCake_likeability': likeability_ranking[7],
-        'Bee_likeability': likeability_ranking[8],
-        'Mermaid_likeability': likeability_ranking[9],
-        'Flower_likeability': likeability_ranking[10],
-        'Spider_likeability': likeability_ranking[11],
-        'Leaf_likeability': likeability_ranking[12],
-        'Pizza_likeability': likeability_ranking[13],
-        'Face_likeability': likeability_ranking[14],
-        'Bus_likeability': likeability_ranking[15],
-        'PalmThree_likeability': likeability_ranking[16]
-    }
-
-    time_stroke_results = {
-        'Ambulance_latency': latency_time[0],
-        'Tractor_latency': latency_time[1],
-        'Owl_latency': latency_time[2],
-        'Train_latency': latency_time[3],
-        'Sheep_latency': latency_time[4],
-        'LightBulb_latency': latency_time[5],
-        'Lion_latency': latency_time[6],
-        'BirthdayCake_latency': latency_time[7],
-        'Bee_latency': latency_time[8],
-        'Mermaid_latency': latency_time[9],
-        'Flower latency': latency_time[10],
-        'Spider_latency': latency_time[11],
-        'Leaf_latency': latency_time[12],
-        'Pizza_latency': latency_time[13],
-        'Face_latency': latency_time[14],
-        'Bus_latency': latency_time[15],
-        'PalmThree_latency': latency_time[16],
-
-        'Ambulance_total_time': total_drawing_time[0],
-        'Tractor_total_time': total_drawing_time[1],
-        'Owl_total_time': total_drawing_time[2],
-        'Train_total_time': total_drawing_time[3],
-        'Sheep_total_time': total_drawing_time[4],
-        'LightBulb_total_time': total_drawing_time[5],
-        'Lion_total_time': total_drawing_time[6],
-        'BirthdayCake_total_time': total_drawing_time[7],
-        'Bee_total_time': total_drawing_time[8],
-        'Mermaid_total_time': total_drawing_time[9],
-        'Flower_total_time': total_drawing_time[10],
-        'Spider_total_time': total_drawing_time[11],
-        'Leaf_total_time': total_drawing_time[12],
-        'Pizza_total_time': total_drawing_time[13],
-        'Face_total_time': total_drawing_time[14],
-        'Bus_total_time': total_drawing_time[15],
-        'PalmThree_total_time': total_drawing_time[16],
-
-        'Ambulance_total_strokes': number_of_strokes[0],
-        'Tractor_total_strokes': number_of_strokes[1],
-        'Owl_total_strokes': number_of_strokes[2],
-        'Train_total_strokes': number_of_strokes[3],
-        'Sheep_total_strokes': number_of_strokes[4],
-        'LightBulb_total_strokes': number_of_strokes[5],
-        'Lion_total_strokes': number_of_strokes[6],
-        'BirthdayCake_total_strokes': number_of_strokes[7],
-        'Bee_total_strokes': number_of_strokes[8],
-        'Mermaid_total_strokes': number_of_strokes[9],
-        'Flower_total_strokes': number_of_strokes[10],
-        'Spider_total_strokes': number_of_strokes[11],
-        'Leaf_total_strokes': number_of_strokes[12],
-        'Pizza_total_strokes': number_of_strokes[13],
-        'Face_total_strokes': number_of_strokes[14],
-        'Bus_total_strokes': number_of_strokes[15],
-        'PalmThree_total_strokes': number_of_strokes[16]
-    }
-
-    dels_data = {
-        'Ambulance': [[difficulty_ranking[0], enjoyment_ranking[0], likeability_ranking[0], number_of_strokes[0]]],
-        'Tractor': [[difficulty_ranking[1], enjoyment_ranking[1], likeability_ranking[1], number_of_strokes[1]]],
-        'Owl': [[difficulty_ranking[2], enjoyment_ranking[2], likeability_ranking[2], number_of_strokes[2]]],
-        'Train': [[difficulty_ranking[3], enjoyment_ranking[3], likeability_ranking[3], number_of_strokes[3]]],
-        'Sheep': [[difficulty_ranking[4], enjoyment_ranking[4], likeability_ranking[4], number_of_strokes[4]]],
-        'LightBulb': [[difficulty_ranking[5], enjoyment_ranking[5], likeability_ranking[5], number_of_strokes[5]]],
-        'Lion': [[difficulty_ranking[6], enjoyment_ranking[6], likeability_ranking[6], number_of_strokes[6]]],
-        'BirthdayCake': [[difficulty_ranking[7], enjoyment_ranking[7], likeability_ranking[7], number_of_strokes[7]]],
-        'Bee': [[difficulty_ranking[8], enjoyment_ranking[8], likeability_ranking[8], number_of_strokes[8]]],
-        'Mermaid': [[difficulty_ranking[9], enjoyment_ranking[9], likeability_ranking[9], number_of_strokes[9]]],
-        'Flower': [[difficulty_ranking[10], enjoyment_ranking[10], likeability_ranking[10], number_of_strokes[10]]],
-        'Spider': [[difficulty_ranking[11], enjoyment_ranking[11], likeability_ranking[11], number_of_strokes[11]]],
-        'Leaf': [[difficulty_ranking[12], enjoyment_ranking[12], likeability_ranking[12], number_of_strokes[12]]],
-        'Pizza': [[difficulty_ranking[13], enjoyment_ranking[13], likeability_ranking[13], number_of_strokes[13]]],
-        'Face': [[difficulty_ranking[14], enjoyment_ranking[14], likeability_ranking[14], number_of_strokes[14]]],
-        'Bus': [[difficulty_ranking[15], enjoyment_ranking[15], likeability_ranking[15], number_of_strokes[15]]],
-        'PalmThree': [[difficulty_ranking[16], enjoyment_ranking[16], likeability_ranking[16], number_of_strokes[16]]]
-    }
-
-    # Create a data frame from the results
-    df_art = pd.DataFrame(art_results, index=[participant])
-
-    df_art.to_csv(art_data_path, mode='a', header=True)
-
-    # Create a data frame from the results
-    df_rank = pd.DataFrame(rank_results, index=[participant])
-
-    df_rank.to_csv(rank_data_path, mode='a', header=True)
-
-    # Create a data frame from the results
-    df_time = pd.DataFrame(time_stroke_results, index=[participant])
-
-    df_time.to_csv(time_data_path, mode='a', header=True)
-
-    # Create a data frame with all the info about difficulty, enjoyment etc.
-    df_dels = pd.DataFrame(dels_data)
-
-    df_dels.to_csv(dels_file, mode='w', header=True)
 
     path_var = code_path + "/artistic_skills.ndjson"
 
@@ -846,21 +689,21 @@ def main():
     if os.path.isfile(path_var):
         with open(path_var) as f:
             data = []
-    reader = ndjson.reader(f)
-    for i in reader:
-        data.append(i)
-    data.append(artistic_data)
+            reader = ndjson.reader(f)
+            for i in reader:
+                data.append(i)
+        data.append(artistic_data)
 
-    # Writing items to a ndjson file
-    with open(path_var, 'w') as f:
-        writer = ndjson.writer(f, ensure_ascii=False)
-    for d in data:
-        writer.writerow(d)
+        # Writing items to a ndjson file
+        with open(path_var, 'w') as f:
+            writer = ndjson.writer(f, ensure_ascii=False)
+            for d in data:
+                writer.writerow(d)
 
     else:
         open(path_var, "x")
-    with open(path_var, "w") as file:
-        json.dump(artistic_data, file)
+        with open(path_var, "w") as file:
+            json.dump(artistic_data, file)
 
     setmode(1)
 
